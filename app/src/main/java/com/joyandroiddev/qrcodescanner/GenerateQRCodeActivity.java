a037ebdb02b577c884072546575197112284b056
Dev-KoushikDas <das.koushik2609@gmail.com>
2022-07-21 21:35:40 +0530

    first commit

---
+++ app/src/main/java/com/joyandroiddev/qrcodescanner/GenerateQRCodeActivity.java
@@ -0,0 +1,14 @@
+package com.joyandroiddev.qrcodescanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GenerateQRCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_qrcode);
+    }
+}
